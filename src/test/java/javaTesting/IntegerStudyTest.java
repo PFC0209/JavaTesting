52f4c38578de8c2944c004d1a281c538b9770350
Pau Febrer Castro <pfebrerc@gmail.com>
2025-04-30 16:24:51 +0200

    creació de

--- src/test/java/javaTesting/IntegerStudyTest.java
+++ src/test/java/javaTesting/IntegerStudyTest.java
@@ -3,6 +3,7 @@ package javaTesting;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class IntegerStudyTest {
 
@@ -13,11 +14,41 @@ public class IntegerStudyTest {
         Integer vint = new Integer(20);
         Integer tretze = 13;
         int esperat = 13;
+         int minim = -2147483648;
 
         assertEquals(" El nombre tretze val realment 13 ", esperat, tretze.intValue());
 
         assertEquals(" El nombre VINT val realment 20 ", 20, vint.intValue());
+
+        assertEquals("El valor mínim d'un int és -2147483648", minim, Integer.MIN_VALUE);
     }
 
+    @Test
+    public void integerComparationStudy(){
 
+        Integer vuit = new Integer(8);
+        Integer eight = new Integer(8);
+
+        assertEquals(" El nombre vuit val realment 8 ", 8, vuit.intValue());
+
+        assertEquals(" El nombre eight val realment 20 ", 8, eight.intValue());
+
+        //assertTrue(vuit == eight);
+
+        assertTrue(vuit.equals(eight));
+    }
+
+    @Test
+    public void binaryStudy(){
+
+        Integer dos = new Integer(2);
+        Integer vint = new Integer(20);
+        Integer tretze = 13;
+
+        String tipusPersones = "hi ha" + Integer.toBinaryString(dos) + "de les persones, les que entenen binari i les que NO!";
+
+        assertEquals(tipusPersones, 2, dos.intValue());
+
+        //assertEquals(" El nombre VINT val realment 20 ", 20, vint.intValue());
+    }
 }
